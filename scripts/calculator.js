f42ee79a429256f5abbaac4be8ba672ccf1541f6
D.J. Crosby <taladan@gmail.com>
2023-02-25 19:57:23 -0500

    Create buttons, display and handlers
    Big push here:
    - All buttons are created with appropriate text
    - event handlers for mouse/kb entry of numbers or operators
    - basic styling on display, font style and button presses
    - event handler hooked to special case of 'clear' functionality
    - trapping 'Enter' and 'Escape' keys and map to correct keys (= & C)

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,12 +1,15 @@
 function add(a, b) {
   return a + b;
 }
+
 function subtract(a, b) {
   return a - b;
 }
+
 function multiply(a, b) {
   return a * b;
 }
+
 function divide(a, b) {
   return a / b;
 }
@@ -29,8 +32,65 @@ function operate(operator, a, b) {
   }
 }
 
-/* tests for return values is expected */
-console.log(operate("+", 5, 2));
-console.log(operate("-", 5, 2));
-console.log(operate("*", 5, 2));
-console.log(operate("/", 5, 2));
+/* set up our variables */
+const calculator = document.getElementById("calculator");
+const display = document.getElementById("display");
+const specialsDiv = document.getElementById("specials");
+const numbersDiv = document.getElementById("numbers");
+const operatorsDiv = document.getElementById("operators");
+
+// Buttons
+const buttonsArray = Array.from(document.querySelectorAll("button"));
+const operatorButtonsArray = Array.from(operators.querySelectorAll("button"));
+const numberButtonsArray = Array.from(numbersDiv.querySelectorAll("button"));
+
+/* event handlers */
+//
+//
+// kb operation
+window.addEventListener("keydown", keyHandler);
+window.addEventListener("keyup", keyHandler);
+
+// mouse operation
+calculator.addEventListener("mousedown", mouseHandler);
+calculator.addEventListener("mouseup", mouseHandler);
+
+// return array of all text in all buttons
+const getButtonTexts = function (buttons) {
+  return buttons.map(getText);
+  function getText(button) {
+    return button.textContent;
+  }
+};
+
+buttonTexts = getButtonTexts(buttonsArray);
+buttonTexts.push("Enter");
+
+// kb functionality
+function keyHandler(event) {
+  let key = event.key;
+  let target = event.target;
+  // Catch Enter and map to '=' and Escape to 'c'
+  if (key === "Enter") {
+    key = "=";
+  } else if (key === "Escape") {
+    key = "C";
+    clearDisplay();
+  }
+  if (buttonTexts.includes(key)) {
+    let accumulator = "";
+    buttonsArray[buttonTexts.indexOf(key)].classList.toggle("pressed");
+  }
+}
+
+// mouse functionality
+function mouseHandler(event) {
+  let target = event.target;
+  let buttonText = target.textContent;
+  let buttonType = target.parentNode.id;
+  target.classList.toggle("pressed");
+}
+
+function clearDisplay() {
+  display.textContent = "";
+}
